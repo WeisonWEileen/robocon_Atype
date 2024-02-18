6232404cd8404ec5ebdb7a64e8088724c3ca98a4
weison_pam <weisonweileen@gmail.com>
2024-02-18 15:56:05 +0800

    NEW: this is the first commit/ ADD SD FATFS
    Signed-off-by: weison_pam <weisonweileen@gmail.com>

---
+++ build/SEGGER_RTT_printf.d
@@ -0,0 +1,6 @@
+build/SEGGER_RTT_printf.o: \
+ Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT_printf.c \
+ Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT.h \
+ Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT_Conf.h
+Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT.h:
+Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT_Conf.h:
---
+++ build/bsp_log.d
@@ -0,0 +1,8 @@
+build/bsp_log.o: Src/bsp_log.c Inc/bsp_log.h \
+ Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT.h \
+ Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT_Conf.h \
+ Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT_Conf.h
+Inc/bsp_log.h:
+Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT.h:
+Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT_Conf.h:
+Middlewares/Third_Party/FatFs/src/RTT/SEGGER_RTT_Conf.h:
